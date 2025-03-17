0f2b09e6139bd3bff3fe1aa584bdb1cc1204a892
MariamAmgad-eng <mariamamgad331@gmail.com>
2025-03-17 04:13:45 -0700

    registraion and auth

--- day 3/ITIan/ITIan/urls.py
+++ day 3/ITIan/ITIan/urls.py
@@ -19,10 +19,14 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Add other paths here
+    path('trainee/', include('trainee.urls')),
+    path('course/', include('course.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
 
 if settings.DEBUG:
--- day 3/ITIan/trainee/models.py
+++ day 3/ITIan/trainee/models.py
@@ -7,6 +7,7 @@ class Trainee(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     profile_picture = models.ImageField(upload_to='trainee_pics/', null=True, blank=True)
-
+    phone = models.CharField(max_length=15)
+    course = models.ForeignKey('course.Course', on_delete=models.CASCADE)
     def __str__(self):
         return self.name
